d637d9a8c17a0f685f914247f4f1e8f5647a4a56
Marcon Willian <marconwillian@protonmail.com>
2020-08-08 13:41:19 -0300

    web - Update inputs for Select

--- packages/web/src/pages/TeacherList/index.tsx
+++ packages/web/src/pages/TeacherList/index.tsx
@@ -4,24 +4,46 @@ import { Link } from 'react-router-dom';
 import './styles.css';
 import PageHeader from '../../components/PageHeader';
 import TeacherItem from '../../components/TeacherItem';
+import Input from '../../components/Input';
+import Select from '../../components/Select';
 
 function TeacherList(){
     return (
         <div id="page-teacher-list" className="container">
             <PageHeader title="Estes são os proffys disponíveis.">
                 <form id="search-teachers">
-                    <div className="input-block">
-                        <label htmlFor="subject">Matéria</label>
-                        <input type="text" id="subject" />
-                    </div>
-                    <div className="input-block">
-                        <label htmlFor="week_day">Dia da semana</label>
-                        <input type="text" id="week_day" />
-                    </div>
-                    <div className="input-block">
-                        <label htmlFor="time">Hora</label>
-                        <input type="text" id="time" />
-                    </div>
+                    <Select 
+                        label="Matéria" 
+                        name="subject"
+                        options={[
+                            {value: 'Artes'},
+                            {value: 'Biologia'},
+                            {value: 'Ciências'},
+                            {value: 'Educação física'},
+                            {value: 'Física'},
+                            {value: 'Geografia'},
+                            {value: 'História'},
+                            {value: 'Matemática'},
+                            {value: 'Português'},
+                            {value: 'Química'}
+                        ]}
+                    />
+
+                    <Select 
+                        label="Dia da semana" 
+                        name="week_day"
+                        options={[
+                            {value: '0', label: 'Domingo'},
+                            {value: '1', label: 'Segunda'},
+                            {value: '2', label: 'Terça'},
+                            {value: '3', label: 'Quarta'},
+                            {value: '4', label: 'Quinta'},
+                            {value: '5', label: 'Sexta'},
+                            {value: '6', label: 'Sábado'}
+                        ]}
+                    />
+
+                    <Input name="Hora" label="Hora" type="time"/>
                 </form>
             </PageHeader>
 
