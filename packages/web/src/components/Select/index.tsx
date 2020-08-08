ddbb4c04e86e1d29b0236a936dc5ca12dbea3317
Marcon Willian <marconwillian@protonmail.com>
2020-08-08 13:40:19 -0300

    Add news components for form

---
+++ packages/web/src/components/Select/index.tsx
@@ -0,0 +1,30 @@
+import React, {SelectHTMLAttributes} from 'react';
+
+import './styles.css';
+
+interface Options {
+    label?: string;
+    value: string;
+}
+
+interface SelectProps extends SelectHTMLAttributes<HTMLSelectElement> {
+    label: string;
+    name: string;
+    options: Options[]
+}
+
+const Select: React.FunctionComponent<SelectProps> = ({label, name, options, ...rest})=>{
+    return (
+        <div className="select-block">
+            <label htmlFor={name}>{label}</label>
+            <select id={name} {...rest} >
+                <option value="" disabled selected hidden>Selecione uma opção</option>
+                {options.map(option => {
+                    return <option key={option.value} value={option.value}>{option.label || option.value}</option>
+                })}
+            </select>
+        </div>
+        )
+};
+
+export default Select;
